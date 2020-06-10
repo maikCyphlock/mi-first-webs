75c230a822dd3f20966cb1b1769c0b57382b663a
maikCyphlock <66704744+maikCyphlock@users.noreply.github.com>
2020-06-09 20:38:30 -0400

    This is the explanation of why I am learning to code
    I relied on this to create my project this is not my own code, i just read that and try to understand it and yeah i did it in my first month, it was amazing for me, so I get confidence after that, i did my v1 for text-to-speech by my friends in the university, so I modified by creating new element like english and spanish button, adding  my university's logo

---
+++ text-to-speech-v1/script.js
@@ -0,0 +1,28 @@
+﻿"use strict";
+
+var language1 = document.getElementById('hablaren');
+var language2 = document.getElementById('hablares');
+
+
+language1.addEventListener('click', function () {
+  decir(document.getElementById('texto'));
+});
+
+function decir(texto) {
+  var utterance = new SpeechSynthesisUtterance(texto.value);
+  utterance.lang = 'en';
+  speechSynthesis.speak(utterance);
+} //Español boton //
+
+
+language2.addEventListener('click', function () {
+  decir1(document.getElementById("texto"));
+});
+
+function decir1(texto) {
+  var utterance = new SpeechSynthesisUtterance(texto.value);
+  utterance.lang = 'es';
+  speechSynthesis.speak(utterance);
+} //alemán boton //
+
+
